aaa64cab1c4c73b87e8ef633d972cf51ec9630d0
Marharyta Zazulia <zazmargo@gmail.com>
2024-12-01 19:55:27 -0300

    Solution: fix bugs flake8 one more

--- initial.py
+++
@@ -1,7 +0,0 @@
-import os
-import django
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "car_service.settings")
-
-django.setup()
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-import initial
-
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from car.models import Car
-from car.serializers import CarSerializer
-import io
+import os
+import django
 
 
-def serialize_car_object(car: Car) -> bytes:
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "car_service.settings")
+
+django.setup()
+
+def serialize_car_object(car: "Car") -> bytes:
+    from rest_framework.renderers import JSONRenderer
+    from car.serializers import CarSerializer
     serializer = CarSerializer(car)
     return JSONRenderer().render(serializer.data)
 
 
-def deserialize_car_object(json: bytes) -> Car:
+def deserialize_car_object(json: bytes) -> "Car":
+    from rest_framework.parsers import JSONParser
+    from car.serializers import CarSerializer
+    import io
     stream = io.BytesIO(json)
     data = JSONParser().parse(stream)
     serializer = CarSerializer(data=data)
